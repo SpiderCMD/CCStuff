5140728e65fcfd0849eefbb64c94fffbfc0b45b3
Kaleb Trofatter <kaleb@SpiderCMD.res6.spectrum.com>
2025-05-02 02:59:55 -0400

    ...

--- TownInfo/startup.lua
+++ TownInfo/startup.lua
@@ -1,6 +1,10 @@
+print("Fetching updates...")
 updateFile = fs.open("update.lua", 'r')
 if updateFile then
 	updateFile.close()
 	fs.delete("update.lua")
 end
-shell.run("wget ")
+shell.run("wget https://raw.githubusercontent.com/SpiderCMD/CCStuff/refs/heads/main/TownInfo/update.lua")
+print("Downloading updates...")
+shell.run("updates.lua")
+
--- TownInfo/update.lua
+++ TownInfo/update.lua
@@ -1,1 +1,11 @@
-print("test")
+--startup
+print("Downloading Startup.lua")
+startup = fs.open("startup.lua", "r")
+if startup then
+	startup.close()
+	fs.delete("startup.lua")
+end
+shell.run("wget https://raw.githubusercontent.com/SpiderCMD/CCStuff/refs/heads/main/TownInfo/startup.lua")
+
+--main
+
